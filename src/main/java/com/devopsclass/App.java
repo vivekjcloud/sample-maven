7799d144b98cbfb9aa432d4990416c475b28903c
EC2 Default User <ec2-user@ip-172-31-35-94.ap-south-1.compute.internal>
2021-03-11 09:06:00 +0530

    final changes

--- src/main/java/com/devopsclass/App.java
+++ src/main/java/com/devopsclass/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World agian, from the cli!, A new change added for git jenkins automation testing" );
+        System.out.println( "Hello World agian, from the cli!, A new change added for git jenkins automation testing, A new changeFInal" );
     }
 }
